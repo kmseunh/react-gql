15f6752c18fc2f20ab70a00272920cfd7aafd04a
kmseunh <tmdgus8779@gmail.com>
2024-01-15 17:08:53 +0900

    refactor: update ReactDOM import and ApolloClient configuration

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+
+const client = new ApolloClient({
+    uri: 'https://rickandmortyapi.com/graphql',
+    cache: InMemoryCache(),
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <App />
+        <ApolloProvider client={client}>
+            <App />
+        </ApolloProvider>
     </React.StrictMode>
 );
