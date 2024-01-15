2681f0b0da259a5faecdb1b9a45442d6425d6b40
kmseunh <tmdgus8779@gmail.com>
2024-01-15 18:22:55 +0900

    refactor: update routing in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,18 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import CharactersList from './page/CharactersList';
+import Character from './page/Character';
+import Search from './page/Search';
 
 function App() {
-    const [count, setCount] = useState(0);
-
     return (
-        <>
-            <div>
-                <a href='https://vitejs.dev' target='_blank' rel='noreferrer'>
-                    <img src={viteLogo} className='logo' alt='Vite logo' />
-                </a>
-                <a href='https://react.dev' target='_blank' rel='noreferrer'>
-                    <img
-                        src={reactLogo}
-                        className='logo react'
-                        alt='React logo'
-                    />
-                </a>
-            </div>
-            <h1>Vite + React</h1>
-            <div className='card'>
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.jsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className='read-the-docs'>
-                Click on the Vite and React logos to learn more
-            </p>
-        </>
+        <div className='App'>
+            <Routes>
+                <Route path='/' element={<CharactersList />} />
+                <Route path='/search' Component={Search} />
+                <Route path='/:id' element={<Character />} />
+            </Routes>
+        </div>
     );
 }
 
